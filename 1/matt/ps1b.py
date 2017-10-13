270a310b4f32f7d248bb25ac06c19d6b517fd59d
Matt Downs <matthewrdowns@gmail.com>
2017-10-13 15:03:01 -0400

    matts 1b

--- 1/matt/ps1b.py
+++ 1/matt/ps1b.py
@@ -22,8 +22,39 @@ def dp_make_weight(egg_weights, target_weight, memo = {}):
     
     Returns: int, smallest number of eggs needed to make target weight
     """
-    # TODO: Your code here
-    pass
+    if egg_weights == [] or target_weight == 0:
+        result = (0, ())
+
+    elif egg_weights[0] > target_weight:
+        result = dp_make_weight(egg_weights[1:], target_weight)
+    else:
+        next_egg = egg_weights[0]
+        withVal, withTake = dp_make_weight(egg_weights[1:], target_weight - next_egg)
+        withVal += next_egg
+    # if not egg_weights:
+    #     return max(memo, key=memo.get)
+
+    # consider = (len(egg_weights), target_weight)
+    # try:
+    #     return memo[consider]
+    # except ValueError:
+    #     while egg_weights[-1] > target_weight:
+    #         egg_weights = egg_weights[:-1]
+
+    #     result = dp_make_weight(egg_weights, target_weight - egg_weights[-1]) 
+
+
+
+
+    ### Greedy algorithm
+    # if target_weight == 1:
+    #     return 1
+
+    # while egg_weights[-1] > target_weight:
+    #     egg_weights = egg_weights[:-1]
+
+    # return 1 + dp_make_weight(egg_weights, target_weight - egg_weights[-1]) 
+
 
 # EXAMPLE TESTING CODE, feel free to add more if you'd like
 if __name__ == '__main__':
@@ -33,4 +64,3 @@ if __name__ == '__main__':
     print("n = 99")
     print("Expected ouput: 9 (3 * 25 + 2 * 10 + 4 * 1 = 99)")
     print("Actual output:", dp_make_weight(egg_weights, n))
-    print()
